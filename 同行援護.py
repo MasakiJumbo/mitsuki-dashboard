d06bace13ead8386a7f635ea4e2d7867f0a724ce
MasakiJumbo <166995095+MasakiJumbo@users.noreply.github.com>
2026-03-11 23:22:32 +0900

    Update title and content for dashboard display

--- 同行援護.py
+++ 同行援護.py
@@ -1,12 +1,5 @@
-{\rtf1\ansi\ansicpg932\cocoartf2867
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+import streamlit as st
 
-\f0\fs24 \cf0 import streamlit as st\
-\
-st.title("\uc0\u21516 \u34892 \u25588 \u35703 \u12480 \u12483 \u12471 \u12517 \u12508 \u12540 \u12489 ")\
-\
-st.write("\uc0\u12371 \u12371 \u12395 \u21516 \u34892 \u25588 \u35703 \u12398 \u20104 \u32004 \u12539 \u22770 \u19978 \u12539 \u31292 \u20685 \u29575 \u12394 \u12393 \u12434 \u34920 \u31034 \u12375 \u12414 \u12377 ")}
+st.title("同行援護ダッシュボード")
+
+st.write("ここに同行援護KPIを表示します")
